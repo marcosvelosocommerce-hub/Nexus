5c51f58a0914590e22c26a90f6dc10226bdd4869
Marcos Veloso <marcosvelosocommerce@email.com>
2026-02-12 21:16:12 -0300

    atualizacoes

--- src/pages/FinancePage.tsx
+++ src/pages/FinancePage.tsx
@@ -94,7 +94,7 @@ const FinancePage = () => {
       return;
     }
 
-    const incomeKeywords = ['ganhei', 'salário', 'salario', 'recebi', 'pix recebido', 'venda', 'vendi', 'renda', 'entrada', 'faturei', "ganha", "recebe", "entrou", "ganha", "entra"];
+    const incomeKeywords = ['ganhei', 'salário', 'salario', 'recebi', 'pix recebido', 'venda', 'vendi', 'renda', 'auxilio', 'auxílio', 'entrada', 'faturei', "ganha", "recebe", "entrou", "ganha", "entra"];
     const isIncome = incomeKeywords.some(key => text.includes(key));
 
     let category = "Geral";
